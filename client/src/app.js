810ee3ec00eec2814c465925959e27d979b2fc14
shankarvarat <shankarvarat@gmail.com>
2021-01-11 18:32:39 +0530

    Add Post  list

--- client/src/app.js
+++ client/src/app.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import PostCreate from './postCreate'
+import PostList from './postList'
 export default () => {
     return <div className='container'>
         <h1>Create Post</h1>
         <PostCreate />
+        <hr />
+        <h1>Posts</h1>
+
+        <PostList />
     </div>
 }
--- post/index.js
+++ post/index.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
 
-var  bodyParser  = require('body-parser')
+var bodyParser = require('body-parser')
 
+const cors = require('cors');
 const app = express();
 const post = {};
 
 app.use(bodyParser.json());
+app.use(cors);
 
 app.get('/post', (req, res) => {
     res.status(201).send(post)
 
- })
+})
 
 app.post('/post', (req, res) => {
-console.log("its working!!")
+    console.log("its working!!")
     const id = randomBytes(4).toString('hex');
     const title = req.body
     post[id] = { id, title }
@@ -23,4 +25,4 @@ console.log("its working!!")
 
 })
 
-app.listen(4000, () => { console.log("Listing on 4000!!") })
+app.listen(4002, () => { console.log("Listing on 4002!!") })
