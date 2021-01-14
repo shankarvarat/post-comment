9739fd6804cc484c4cc9960fa28c5b264fb6d1df
shankarvarat <shankarvarat@gmail.com>
2021-01-14 13:15:19 +0530

    Update in Comments List

--- client/src/commentsList.js
+++ client/src/commentsList.js
@@ -2,22 +2,23 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default ({ postId }) => {
-    const [comments, setComments] = useState([]);
+  const [comments, setComments] = useState([]);
 
-    const fetchData = async () => {
-        const res = await axios.get(
-            'http://localhost:4001/posts/${postId}/commnets'
-        );
+  const fetchData = async () => {
+    const res = await axios.get(
+      `http://localhost:4001/posts/${postId}/comments`
+    );
 
-        setComments(res.data);
-    };
+    setComments(res.data);
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const renderedComments = comments.map(comment => {
-        return <li key={comment.id}>{comment.content}</li>;
-    });
-    return <ul> {renderedComments}</ul>
-}
+  const renderedComments = comments.map(comment => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
